2013bcf74b580d76658de902f1516c04d5ef6035
Gerardo Belot <gerardo.belot@gmail.com>
2019-09-08 12:18:21 -0600

    Creando archivos de configuración de spring security

---
+++ spring-security-demo-01-base-app/src/main/java/com/gbelot/demo/DemoSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.gbelot.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		
+	}
+	
+}
