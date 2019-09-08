4ba67bb709c328ac785a10623de334f5549c78bc
Gerardo Belot <gerardo.belot@gmail.com>
2019-09-08 12:21:37 -0600

    Agregando usuarios en memory para pruebas

--- spring-security-demo-01-base-app/src/main/java/com/gbelot/demo/DemoSecurityConfig.java
+++ spring-security-demo-01-base-app/src/main/java/com/gbelot/demo/DemoSecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
 @EnableWebSecurity
@@ -11,6 +13,13 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+			.withUser(users.username("gerardo").password("test123").roles("EMPLOYEE"))
+			.withUser(users.username("samu").password("test123").roles("MANAGER"))
+			.withUser(users.username("nely").password("test123").roles("ADMIN"));
 		
 		
 	}
